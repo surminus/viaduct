8b442f7a6a1bec3e88bdcda0b84c75dc8c5f2464
Laura Martin <surminus@gmail.com>
2021-10-15 20:30:38 +0100

    Actually implement file functions
    I removed user and group for now because I don't need them, but this
    implements creation and deletion of files.

--- example/main.go
+++ example/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/surminus/viaduct"
 )
 
 func main() {
-	viaduct.File{Path: "bar"}.Create()
-
-	fmt.Println(viaduct.Attribute.User)
+	viaduct.File{Path: "foo", Content: "bar"}.Create()
 }
--- file.go
+++ file.go
@@ -1,38 +1,29 @@
 package viaduct
 
 import (
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 // File manages files on the filesystem
 type File struct {
 	Path    string
 	Content string
-	Mode    string
-	Owner   string
-	Group   string
+	Mode    os.FileMode
 }
 
 // satisfy sets default values for the parameters for a particular
 // resource
 func (f *File) satisfy() {
-	// Set required values here, and error if they are not
-	// set
+	// Set required values here, and error if they are not set
 	if f.Path == "" {
 		log.Fatal("Path is a required parameter")
 	}
 
-	// Set optional defaults here, or leave them empty if that's required
-	if f.Mode == "" {
-		f.Mode = "0644"
-	}
-
-	if f.Owner == "" {
-		f.Owner = "root"
-	}
-
-	if f.Group == "" {
-		f.Group = "root"
+	// Set optional defaults here
+	if f.Mode == 0 {
+		f.Mode = 0644
 	}
 }
 
@@ -40,10 +31,16 @@ func (f *File) satisfy() {
 func (f File) Create() (err error) {
 	f.satisfy()
 
+	err = ioutil.WriteFile(f.Path, []byte(f.Content), f.Mode)
+
 	return err
 }
 
 // Delete deletes a file
 func (f *File) Delete() (err error) {
+	f.satisfy()
+
+	err = os.Remove(f.Path)
+
 	return err
 }
