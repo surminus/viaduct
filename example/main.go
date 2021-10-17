7cec627d76bdbc37edcad74ab96b020fafc7f23f
Laura Martin <surminus@gmail.com>
2021-10-17 14:52:30 +0100

    WIP! templates
    This broadly shows how to create a template.
    
    Given that it's quite lengthy, I should build it straight into a
    `template` resource.
    
    The slightly tricky thing here is the data we're passing in.

--- example/main.go
+++ example/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"bytes"
+	"embed"
 	"fmt"
+	"log"
+	"text/template"
 
 	"github.com/surminus/viaduct"
 )
 
+//go:embed templates
+var templates embed.FS
+
+type TmplData struct {
+	Name string
+}
+
 func main() {
+	testFile, err := templates.ReadFile("templates/test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t, err := template.New("test").Parse(string(testFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := TmplData{Name: "Laura"}
+	var b bytes.Buffer
+	err = t.Execute(&b, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	dir := viaduct.Directory{Path: "test"}.Create()
-	viaduct.File{Path: fmt.Sprintf("%s/foo", dir.Path), Content: "bar"}.Create()
-
-	g := viaduct.Git{
-		Path: "/tmp/viaduct",
-		URL:  "https://github.com/surminus/viaduct",
-	}.Create()
-
-	fmt.Println(viaduct.Attribute.JSON())
-
-	g.Delete()
-	dir.Delete()
+	viaduct.File{Path: fmt.Sprintf("%s/foo", dir.Path), Content: b.String()}.Create()
 }
