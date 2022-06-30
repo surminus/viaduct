e20a3bf278ae8b727551798febb4d88c71f103d2
Laura Martin <laura@surminus.com>
2022-06-30 10:22:49 +0100

    Provide directory resource with file attributes
    This just copy and pastes the stuff in the file resource to do with user
    permissions.
    
    Ideally I would refactor this to cut out the repetition, but for now I'm
    just ensuring they are set.

--- directory.go
+++ directory.go
@@ -3,12 +3,25 @@ package viaduct
 import (
 	"log"
 	"os"
+	"os/user"
+	"strconv"
 )
 
 // Directory manages a directory on the filesystem
 type Directory struct {
+	// Path is the path of the directory
 	Path string
+	// Mode is the permissions set of the directory
 	Mode os.FileMode
+
+	// User sets the user permissions by user name
+	User string
+	// Group sets the group permissions by group name
+	Group string
+	// UID sets the user permissions by UID
+	UID int
+	// GID sets the group permissions by GID
+	GID int
 }
 
 // satisfy sets default values for the parameters for a particular
@@ -23,6 +36,22 @@ func (d *Directory) satisfy() {
 	if d.Mode == 0 {
 		d.Mode = 0o755
 	}
+
+	if d.User == "" && d.UID == 0 {
+		if uid, err := strconv.Atoi(Attribute.User.Uid); err != nil {
+			log.Fatalf("==> Directory [error] Internal error: %s", err)
+		} else {
+			d.UID = uid
+		}
+	}
+
+	if d.Group == "" && d.GID == 0 {
+		if gid, err := strconv.Atoi(Attribute.User.Gid); err != nil {
+			log.Fatalf("==> Directory [error] Internal error: %s", err)
+		} else {
+			d.GID = gid
+		}
+	}
 }
 
 // Create creates a directory
@@ -34,7 +63,41 @@ func (d Directory) Create() *Directory {
 		return &d
 	}
 
-	err := os.MkdirAll(HelperExpandPath(d.Path), d.Mode)
+	path := HelperExpandPath(d.Path)
+
+	err := os.MkdirAll(HelperExpandPath(path), d.Mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	uid := d.UID
+	gid := d.GID
+
+	if d.User != "" {
+		u, err := user.Lookup(d.User)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		uid, err = strconv.Atoi(u.Uid)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if d.Group != "" {
+		g, err := user.LookupGroup(d.Group)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		gid, err = strconv.Atoi(g.Gid)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	err = os.Chown(path, uid, gid)
 	if err != nil {
 		log.Fatal(err)
 	}
